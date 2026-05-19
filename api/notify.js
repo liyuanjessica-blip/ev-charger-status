5c991c8cbaa979c55a6a579e94f0a7775602353a
liyuanjessica-blip <liyuanjessica@gmail.com>
2026-05-19 16:59:29 -0700

    Update notify.js

--- api/notify.js
+++ api/notify.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
     const response = await fetch(process.env.SLACK_WEBHOOK_URL, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ message: message })，
+      body: JSON.stringify({ message })，
     });
 
     const text = await response.text();
